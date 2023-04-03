0656156c44c04466383e1bc72627690d88927f32
AgustinMineo <mineoagustin@gmail.com>
2023-04-03 20:42:05 -0300

    more implementations

--- js/main.js
+++ js/main.js
@@ -5,7 +5,17 @@ const desktopMenu = document.querySelector('.desktop-menu');
 navEmail.addEventListener('click', toggleDesktopMenu);
 // Remueve o agrega la clase inactive del menu-desktop para mostrar o no el menu.
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+    const asideClose = aside.classList.contains('inactive');
+    const desktopMenuOpen = desktopMenu.classList.contains('inactive');
+    if(!asideClose){
+        aside.classList.add('inactive');
+    }
+
+    if(desktopMenuOpen){
+        desktopMenu.classList.toggle('inactive');
+    }else{
+        desktopMenu.classList.add('inactive')
+    }
 }
 // DesktopMenu
 
@@ -15,5 +25,28 @@ const mobileMenu = document.querySelector('.mobile-menu')
 burguerMenu.addEventListener('click',toggleMobileMenu);
 
 function toggleMobileMenu(){
+    const asideClose = aside.classList.contains('inactive');
+    if(!asideClose){
+        aside.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
+    
+}
+
+/*Aside */
+const navBarShop = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail-inactive');
+
+navBarShop.addEventListener('click',toggleShoppingCart);
+
+function toggleShoppingCart(){
+    const MobileMenuClose = mobileMenu.classList.contains('inactive');
+
+    if(!MobileMenuClose){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+
+    
 }
