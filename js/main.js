15c4c77090cfecd94d3648d6697877b473d13559
AgustinMineo <mineoagustin@gmail.com>
2023-04-06 17:32:31 -0300

    more styles fix

--- js/main.js
+++ js/main.js
@@ -59,17 +59,20 @@ const productList=[];
 productList.push({
     name:'Bike',
     price:120,
-    image: 'https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=360&h=420&dpr=1'
+    image: 'https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=360&h=420&dpr=1',
+    description:'asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasddsasadadsadsadslkadsadslkdkajshasdhshakdasdasdsadasdasdas1'
 });
 productList.push({
     name:'Mouse',
     price:250,
-    image: 'https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=360&h=420&dpr=1'
+    image: 'https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=360&h=420&dpr=1',
+    description:'asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasddsasadadsadsadslkadsadslkdkajshasdhshakdasdasdsadasdasdas2'
 });
 productList.push({
     name:'Notebook',
     price:1200,
-    image: 'https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=360&h=420&dpr=1'
+    image: 'https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=360&h=420&dpr=1',
+    description:'asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasddsasadadsadsadslkadsadslkdkajshasdhshakdasdasdsadasdasdas3'
 });
 
 
@@ -82,6 +85,7 @@ for (product of arr){
 
     const productImg = document.createElement('img');
     productImg.setAttribute('src',product.image);
+    productImg.addEventListener('click',openProductDetail);
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-details');
@@ -124,3 +128,94 @@ function toggleCloseProductDetail(){
     }
     statusMainContainerAsideDetail.classList.toggle('inactive');
 }
+
+function openProductDetail(){
+    statusMainContainerAsideDetail.classList.remove('inactive');
+}
+
+
+/*Build product info aside */
+
+/*<aside class="product-detail-info ">
+            <img src="https://img.freepik.com/free-psd/furniture-sale-social-media-banner-template_120329-2260.jpg?w=826&t=st=1678314659~exp=1678315259~hmac=7e5ed58c8f7400ef2d50f916a40845a4ca67341aad843d320e417c5420738e1a" alt="">
+            <div class="product-info">
+                <p> $35,00</p>
+                <p>Name product</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur veniam numquam nesciunt tempora necessitatibus sit ut expedita perspiciatis praesentium sapiente nobis sequi repellat, architecto commodi ab tempore voluptatem magnam explicabo?</p>
+                <button class="primary-button add-to-card-button">
+                    <img src="./icons/bt_add_to_cart.svg" alt="Add to cart" srcset="">
+                    Add to card
+                </button>
+            </div>
+        </aside>*/
+/*
+function openProductDetail(img,name,description,price){
+    const productDetailMain = document.querySelector('.mainContainerAsideDetail');
+    
+    const productDetailAsideMain = document.createElement('div');
+    productDetailAsideMain.classList.add('.mainContainerAsideDetail');
+
+    const productDetailAside = document.createElement('aside');
+    productDetailAside.classList.add('product-detail-info');
+
+    const productDetailClose = document.createElement('div');
+    productDetailClose.classList.add('product-detail-close');
+
+    const productDetailCloseImg = document.createElement('img');
+    productDetailCloseImg.setAttribute('src','./icons/icon_close.png')
+    productDetailClose.appendChild(productDetailCloseImg);
+
+    //Product img
+    const productImgDetail = document.createElement('img');
+    productImgDetail.setAttribute('src',img);
+    
+    const productDetailDots = document.createElement('div');
+    productDetailDots.classList.add('dots');
+
+    const productDetailDots1 = document.createElement('span');
+    productDetailDots1.classList.add('dot');
+    const productDetailDots2 = document.createElement('span');
+    productDetailDots2.classList.add('dot');
+    const productDetailDots3 = document.createElement('span');
+    productDetailDots3.classList.add('dot');
+
+    productDetailDots.appendChild(productDetailDots1)
+    productDetailDots.appendChild(productDetailDots2)
+    productDetailDots.appendChild(productDetailDots3)
+
+
+    const productInfoDetail = document.createElement('div');
+    productInfoDetail.classList.add('product-info');
+
+    const productInfoDetailPrice = document.createElement('p');
+    productInfoDetailPrice.innerText= '$' + price;
+    const productInfoDetailName = document.createElement('p');
+    productInfoDetailName.innerText= name;
+    const productInfoDetailDescription = document.createElement('p');
+    productInfoDetailDescription.innerText= description;
+    productInfoDetail.appendChild(productInfoDetailPrice);
+    productInfoDetail.appendChild(productInfoDetailName);
+    productInfoDetail.appendChild(productInfoDetailDescription);
+    
+    const productInfoDetailButton = document.createElement('button');
+    productInfoDetailButton.classList.add('primary-button');
+    productInfoDetailButton.classList.add('add-to-card-button');
+    
+    const productInfoDetailButtonImg = document.createElement('img');
+    productInfoDetailButtonImg.setAttribute('src','./icons/bt_add_to_cart.svg');
+    productInfoDetailButton.appendChild(productInfoDetailButtonImg)
+    
+    productInfoDetail.appendChild(productInfoDetailButton);
+
+    //Add everything to the aside
+    productDetailAside.appendChild(productDetailClose);
+    productDetailAside.appendChild(productImgDetail);
+    productDetailAside.appendChild(productDetailDots);
+    productDetailAside.appendChild(productInfoDetail);
+
+    productDetailAsideMain.appendChild(productDetailAside);
+    
+    productDetailMain.appendChild(productDetailAside);
+    //productCardContainers.appendChild(productCard);
+
+}*/
